a54bacc526674fc42887bb394853b389b10258f9
Mark-Finley <moseihyiaman.ac@st.vvu.edu.gh>
2023-11-20 00:15:55 +0000

    test qr  code generator

--- resources/qr-code-gen/qr-code.py
+++ resources/qr-code-gen/qr-code.py
@@ -1,5 +1,7 @@
 import qrcode
 
-student_id = "1234567891"
-generate_qr_code = qrcode.make(student_id)
-generate_qr_code.save("resources/qr-code-gen/image02.png")
+student_id = [221,222,223,224,225,226,227,228,229,230]
+
+for id in student_id:
+    generate_qr_code = qrcode.make(str(id))
+    generate_qr_code.save(f"resources/qr-code-gen/image{id}.png")
