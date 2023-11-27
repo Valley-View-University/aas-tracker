e5504efdda65e596e42c3761832cc73ed252c1e7
Mark-Finley <moseihyiaman.ac@st.vvu.edu.gh>
2023-11-27 23:17:29 +0000

    feature: javascript qr code generator

--- resources/qr-code-gen/qr-code.py
+++ resources/qr-code-gen/qr-code.py
@@ -5,3 +5,4 @@ student_id = [221,222,223,224,225,226,227,228,229,230]
 for id in student_id:
     generate_qr_code = qrcode.make(str(id))
     generate_qr_code.save(f"resources/qr-code-gen/image{id}.png")
+    print(f"QR Code for {id} generated successfully")
