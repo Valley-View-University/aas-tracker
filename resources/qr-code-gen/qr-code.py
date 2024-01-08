2edb105bb04a4a0cb840ab5e4e63b129ef14fb7e
Mark-Finley <moseihyiaman.ac@st.vvu.edu.gh>
2024-01-08 14:07:55 +0000

    final from Dev mark

--- resources/qr-code-gen/qr-code.py
+++
@@ -1,7 +0,0 @@
-import qrcode
-
-student_id = [221,222,223,224,225,226,227,228,229,230]
-
-for id in student_id:
-    generate_qr_code = qrcode.make(str(id))
-    generate_qr_code.save(f"resources/qr-code-gen/image{id}.png")
